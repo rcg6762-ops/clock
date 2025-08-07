04a739ffceb2aaf57ee88e394d6eecdd9ae84fbb
rcg6762-ops <rcg6762@gmail.com>
2025-08-07 17:41:39 +0800

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+// service-worker.js
+const CACHE_NAME = 'ms-clock-v1';
+const CACHE_URLS = [
+    './',
+    './index.html',
+    './icon.svg',
+    './manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(CACHE_URLS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
